4d3d8b08dfc4236d323b66c3955f001ac4ec1c87
fed.aleotti <fed.aleotti@gmail.com>
2020-02-09 18:23:20 +0100

    Aggiunto id a FAKE_CANDIDATES

--- fake_candidates.py
+++ fake_candidates.py
@@ -1,6 +1,16 @@
 import random
 import csv
 import asn
+import string
+
+
+def randomString(stringLength=12):
+    letters = list(map(chr, range(97, 123)))
+    val = ""
+    for _ in range(stringLength):
+        val = val + random.choice(letters)
+    return val
+
 
 candidates = {}
 candidateIndex = 0
@@ -10,9 +20,11 @@ with open('./data/CANDIDATES_OUT.csv') as document:
     count = 0
     for row in reader:
         count = count + 1
+        candidateId = randomString()
         # subject = random.choice(['INFORMATICA', 'GEOMETRIA E ALGEBRA',
         #                          'RICERCA OPERATIVA', 'CHIMICA INDUSTRIALE', 'ELETTRONICA'])
-        candidates[candidateIndex] = {"name": row[0], "level": row[1], "session": row[2], "subject": 'INFORMATICA', "journalDois": row[3], "dois": row[4]}
+        candidates[candidateIndex] = {"id": candidateId, "name": row[0], "level": row[1], "session": row[2],
+                                      "subject": 'INFORMATICA', "journalDois": row[3], "dois": row[4]}
         candidateIndex = candidateIndex + 1
-asn.createCSV(candidates, './data/FAKE_CANDIDATES_OUT.csv', ["name", "level", "session", "subject", "journalDois", "dois"], 0)
-
+asn.createCSV(candidates, './data/FAKE_CANDIDATES_OUT.csv',
+              ["id", "name", "level", "session", "subject", "journalDois", "dois"], 0)
